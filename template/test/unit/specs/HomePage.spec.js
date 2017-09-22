2423730267872b8bdcbb79f910772ae6cb3b7fa9
Travis Horn <horn.travis@gmail.com>
2017-09-22 15:58:17 -0500

    Favor showing tests as comments
    to reduce the amount of deleting/rewriting of template code.

--- template/test/unit/specs/HomePage.spec.js
+++ template/test/unit/specs/HomePage.spec.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
-import Hello from '@/components/Hello'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import HomePage from '@/components/HomePage'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
-describe('Hello.vue', () => {
+describe('HomePage.vue', () => {
+  /* Describe tests as follows...
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(Hello){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+    const Constructor = Vue.extend(HomePage){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
     const vm = new Constructor().$mount(){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+
     expect(vm.$el.querySelector('.hello h1').textContent)
       .to.equal('Welcome to Your Vue.js App'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
   }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  */
 }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
